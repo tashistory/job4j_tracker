7233db3b46bf1b71063a4cfbf70ab707bfef5bd8
semyon <semyon.mihalich@gmail.com>
2023-03-15 13:34:36 +0500

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/tracker/opp/Error.java
@@ -0,0 +1,29 @@
+package ru.job4j.tracker.opp;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void getCondition() {
+        System.out.println("active: " + this.active);
+        System.out.println("status: " + this.status);
+        System.out.println("message: " + this.message);
+    }
+
+    public static void main(String[] args) {
+        Error first = new Error();
+        Error second = new Error(true, 2, "Permission denied");
+        first.getCondition();
+        second.getCondition();
+    }
+}
