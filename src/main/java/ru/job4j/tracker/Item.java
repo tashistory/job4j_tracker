cebc9a3880276341ea91cac0cd2df08cf96329e4
semyon <semyon.mihalich@gmail.com>
2023-03-16 16:58:50 +0500

    2. Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,15 @@ public class Item {
     private int id;
     private String name;
 
+    public Item(int id, String name) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
     public int getId() {
         return id;
     }
