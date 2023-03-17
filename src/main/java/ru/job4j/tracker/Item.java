c638ff776396621d72409d863990980f1b41b8b1
semyon <semyon.mihalich@gmail.com>
2023-03-17 11:06:47 +0500

    8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,11 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item(int id, String name) {
         this.name = name;
@@ -26,6 +29,10 @@ public class Item {
 
     public String getName() {
         return name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 
     public void setName(String name) {
