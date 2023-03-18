6c6f7c3e1d753a469f4a55379cc07eb541479e4c
semyon <semyon.mihalich@gmail.com>
2023-03-18 13:59:45 +0500

    5. Tracker - хранилище [#396 #401810]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -3,8 +3,7 @@ package ru.job4j.tracker;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
+
 
 public class TrackerTest {
     @Test
@@ -14,7 +13,7 @@ public class TrackerTest {
         item.setName("test1");
         tracker.add(item);
         Item result = tracker.findById(item.getId());
-        assertThat(result.getName(), is(item.getName()));
+        assertThat(result.getName()).isEqualTo(item.getName());
     }
 
     @Test
