4593a6aa8fa28e52bbc1eb69851e559bdaa0d0b9
semyon <semyon.mihalich@gmail.com>
2023-03-21 18:58:33 +0500

    11.1. Реализация меню за счет шаблона стратегия. [#181780]

---
+++ src/main/java/ru/job4j/tracker/StartUITest.java
@@ -0,0 +1,30 @@
+package ru.job4j.tracker;
+
+public class StartUITest {
+    public void init(Input input, Tracker tracker, UserAction[] actions) {
+        boolean run = true;
+        while (run) {
+            showMenu(actions);
+            int select = input.askInt("Select: ");
+            UserAction action = actions[select];
+            run = action.execute(input, tracker);
+        }
+    }
+
+    private void showMenu(UserAction[] actions) {
+        System.out.println("Menu.");
+        for (int index = 0; index < actions.length; index++) {
+            System.out.println(index + ". " + actions[index].name());
+        }
+    }
+
+    public static void main(String[] args) {
+        Input input = new ConsoleInput();
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new CreateAction(), new DeleteAction(), new EditAction(), new ShowAction(),
+                new FindActionById(), new FindActionByName(), new ExitAction()
+        };
+        new StartUITest().init(input, tracker, actions);
+    }
+}
