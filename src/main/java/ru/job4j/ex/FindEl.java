f09e85479b05d61a5da9ae1cd7a45519c1039008
semyon <semyon.mihalich@gmail.com>
2023-03-22 16:11:49 +0500

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,26 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (key.equals(value[i])) {
+                rsl = i;
+                break;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Element Not Found");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) throws ElementNotFoundException {
+        String[] value = {"Petr", "Ivan", "Nik", "Vasya"};
+        try {
+            System.out.println(indexOf(value, "Test"));
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
