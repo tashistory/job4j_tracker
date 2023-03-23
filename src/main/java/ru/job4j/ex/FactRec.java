da4a3abf99900076dc6811f97f29d4fe1e234add
semyon <semyon.mihalich@gmail.com>
2023-03-23 13:14:27 +0500

    0.6. Error - исключения, связанные с работой виртуальной машины [#219370 #403238]

--- src/main/java/ru/job4j/ex/FactRec.java
+++ src/main/java/ru/job4j/ex/FactRec.java
@@ -5,7 +5,7 @@ public class FactRec {
         if (n == 0 || n == 1) {
             return 1;
         }
-            return calc(n - 1) * n;
+        return calc(n - 1) * n;
     }
 
     public static void main(String[] args) {
