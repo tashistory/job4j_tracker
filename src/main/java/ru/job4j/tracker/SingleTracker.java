1fb58d9559013adfbdf6ec32ccbc5ead0103b10b
semyon <semyon.mihalich@gmail.com>
2023-03-24 12:01:45 +0500

    1. Singleton [#94606 #403727]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -8,10 +8,6 @@ public class SingleTracker {
 
     private static SingleTracker instance = null;
 
-    private final Item[] items = new Item[100];
-    private int ids = 1;
-    private int size = 0;
-
     private SingleTracker() {
     }
 
@@ -22,62 +18,27 @@ public class SingleTracker {
         return instance;
     }
 
-    private int indexOf(int id) {
-        int rsl = -1;
-        for (int index = 0; index < size; index++) {
-            if (items[index].getId() == id) {
-                rsl = index;
-                break;
-            }
-        }
-        return rsl;
-    }
-
     public Item add(Item item) {
-        item.setId(ids++);
-        items[size++] = item;
-        return item;
+        return tracker.add(item);
     }
 
     public Item findById(int id) {
-        int index = indexOf(id);
-        return index != -1 ? items[index] : null;
+        return tracker.findById(id);
     }
 
     public boolean replace(int id, Item item) {
-        int i = indexOf(id);
-        boolean rsl = i != -1;
-        if (rsl) {
-            item.setId(id);
-            items[i] = item;
-        }
-        return rsl;
+        return tracker.replace(id, item);
     }
 
     public Item[] findAll() {
-        return Arrays.copyOf(items, size);
+        return tracker.findAll();
     }
 
     public Item[] findByName(String key) {
-        Item[] rsl = new Item[size];
-        int k = 0;
-        for (int i = 0; i < size; i++) {
-            if (key.equals(items[i].getName())) {
-                rsl[k] = items[i];
-                k++;
-            }
-        }
-        return Arrays.copyOf(rsl, k);
+        return tracker.findByName(key);
     }
 
     public boolean delete(int id) {
-        int i = indexOf(id);
-        boolean rsl = i != -1;
-        if (rsl) {
-            System.arraycopy(items, i + 1, items, i, size - i - 1);
-            items[size - 1] = null;
-            size--;
-        }
-        return rsl;
+        return tracker.delete(id);
     }
 }
