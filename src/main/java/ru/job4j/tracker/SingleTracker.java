b7188b4d7d367ab39caf1b9507b2a1f43386c91f
semyon <semyon.mihalich@gmail.com>
2023-03-24 11:40:54 +0500

    1. Singleton [#94606 #403727]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -1,10 +1,16 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class SingleTracker {
 
     private Tracker tracker = new Tracker();
 
     private static SingleTracker instance = null;
+
+    private final Item[] items = new Item[100];
+    private int ids = 1;
+    private int size = 0;
 
     private SingleTracker() {
     }
@@ -16,11 +22,62 @@ public class SingleTracker {
         return instance;
     }
 
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
+
     public Item add(Item item) {
-        return tracker.add(item);
+        item.setId(ids++);
+        items[size++] = item;
+        return item;
     }
 
     public Item findById(int id) {
-        return null;
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
+    public boolean replace(int id, Item item) {
+        int i = indexOf(id);
+        boolean rsl = i != -1;
+        if (rsl) {
+            item.setId(id);
+            items[i] = item;
+        }
+        return rsl;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[size];
+        int k = 0;
+        for (int i = 0; i < size; i++) {
+            if (key.equals(items[i].getName())) {
+                rsl[k] = items[i];
+                k++;
+            }
+        }
+        return Arrays.copyOf(rsl, k);
+    }
+
+    public boolean delete(int id) {
+        int i = indexOf(id);
+        boolean rsl = i != -1;
+        if (rsl) {
+            System.arraycopy(items, i + 1, items, i, size - i - 1);
+            items[size - 1] = null;
+            size--;
+        }
+        return rsl;
     }
 }
