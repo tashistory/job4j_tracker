080bfa3e01617cf6a87b436badfbf686311b2733
semyon <semyon.mihalich@gmail.com>
2023-03-25 10:34:44 +0500

    3. final [#334073 #403784]

--- src/main/java/ru/job4j/ooa/Airbus.java
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -1,7 +1,7 @@
 package ru.job4j.ooa;
 
 public final class Airbus extends Aircraft {
-    private static int COUNT_ENGINE = 2;
+    private final static int COUNT_ENGINE = 2;
 
     private String name;
 
