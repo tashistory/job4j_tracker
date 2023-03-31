ac60c63280293f2b1af70b64310f14db8ff800aa
semyon <semyon.mihalich@gmail.com>
2023-03-31 13:51:54 +0500

    9. Изменить программу Tracker из 2-го модуля [#10039]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,16 +1,19 @@
 package ru.job4j.tracker;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Tracker {
-    private final Item[] items = new Item[100];
+    //private final Item[] items = new Item[100];
+    private final List<Item> items = new ArrayList<>();
     private int ids = 1;
     private int size = 0;
 
     private int indexOf(int id) {
         int rsl = -1;
         for (int index = 0; index < size; index++) {
-            if (items[index].getId() == id) {
+            if (items.get(index).getId() == id) {
                 rsl = index;
                 break;
             }
@@ -20,13 +23,13 @@ public class Tracker {
 
     public Item add(Item item) {
         item.setId(ids++);
-        items[size++] = item;
+        items.add(size++, item);
         return item;
     }
 
     public Item findById(int id) {
         int index = indexOf(id);
-        return index != -1 ? items[index] : null;
+        return index != -1 ? items.get(index) : null;
     }
 
     public boolean replace(int id, Item item) {
@@ -34,34 +37,37 @@ public class Tracker {
         boolean rsl = i != -1;
         if (rsl) {
             item.setId(id);
-            items[i] = item;
+            items.set(i, item);
         }
         return rsl;
     }
 
-    public Item[] findAll() {
-        return Arrays.copyOf(items, size);
+    public List<Item> findAll() {
+        return items;
     }
 
-    public Item[] findByName(String key) {
-        Item[] rsl = new Item[size];
+    public List<Item> findByName(String key) {
+        List<Item> rsl = new ArrayList<>();
         int k = 0;
         for (int i = 0; i < size; i++) {
-            if (key.equals(items[i].getName())) {
-                rsl[k] = items[i];
+            if (key.equals(items.get(i).getName())) {
+               rsl.add(k , items.get(i));
+                // rsl[k] = items[i];
                 k++;
             }
         }
-        return Arrays.copyOf(rsl, k);
+        //return Arrays.copyOf(rsl, k);
+        return rsl;
     }
 
     public boolean delete(int id) {
-        int i = indexOf(id);
-        boolean rsl = i != -1;
-        if (rsl) {
-            System.arraycopy(items, i + 1, items, i, size - i - 1);
-            items[size - 1] = null;
-            size--;
+       int i = indexOf(id);
+       boolean rsl = i != -1;
+       if (rsl) {
+           items.remove(i);
+         //   System.arraycopy(items, i + 1, items, i, size - i - 1);
+          // items[size - 1] = null;
+           // size--;
         }
         return rsl;
     }
