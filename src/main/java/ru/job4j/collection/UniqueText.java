f3e155c0d71580bb42302af4a909e383f6642be1
semyon <semyon.mihalich@gmail.com>
2023-04-03 13:34:33 +0500

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,20 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String o : origin) {
+            check.add(o);
+        }
+        for (String t : text) {
+            rsl = check.contains(t);
+            break;
+        }
+        return rsl;
+    }
+}
