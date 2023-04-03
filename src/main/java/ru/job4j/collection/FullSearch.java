cf4c6766b7f6d4f61f8bca7cf44f24dae90921b4
semyon <semyon.mihalich@gmail.com>
2023-04-03 13:05:42 +0500

    1. Уникальные задачи. [#212653]

--- src/main/java/ru/job4j/collection/FullSearch.java
+++ src/main/java/ru/job4j/collection/FullSearch.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class FullSearch {
     public static HashSet<String> extractNumber(List<Task> list) {
         HashSet<String> numbers = new HashSet<>();
-        for ( Task l : list ) {
+        for (Task l : list) {
             numbers.add(l.getNumber());
         }
         return numbers;
