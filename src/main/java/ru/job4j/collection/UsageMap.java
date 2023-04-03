6c506dff589dca158f192a08124226606039578e
semyon <semyon.mihalich@gmail.com>
2023-04-03 17:37:20 +0500

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("vasiliy@gmail.com", "Vasisliy Петров");
+        map.put("dmitriy@mail.ru", "Dmitriy Kuznetsov");
+        map.put("antoliy@yandex.ru", "Anantoliy Volkov");
+
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(String.format("%-20s %-20s", key , value));
+        }
+    }
+}
