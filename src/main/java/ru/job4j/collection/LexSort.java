b368556a026a529dcc901fc4d0b456e9a99086f8
semyon <semyon.mihalich@gmail.com>
2023-04-06 16:38:53 +0500

    4. Сортировка номера [#221284]

---
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -0,0 +1,13 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String> {
+
+    @Override
+    public int compare(String left, String right) {
+        String[] leftM  = left.split(". ");
+        String[] rightM = right.split(". ");
+        return Integer.compare(Integer.parseInt(leftM[0]), Integer.parseInt(rightM[0]));
+    }
+}
