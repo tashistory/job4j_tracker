5a304d4d32f4d4ac4aae074e97c8b769a5464459
semyon <semyon.mihalich@gmail.com>
2023-04-11 14:55:19 +0500

    3. Отсортировать департаменты [#34352 #408517]

---
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class DepDescComp implements Comparator<String> {
+public int compare(String left, String right) {
+        String[] leftM  = left.split("/");
+        String[] rightM = right.split("/");
+        int rslt = rightM[0].compareTo(leftM[0]);
+        if (rslt == 0) {
+            return left.compareTo(right);
+        }
+        return rslt;
+        }
+}
