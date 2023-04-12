b6570c168b74a58a098eb5a91edf0808a8e31bf3
semyon <semyon.mihalich@gmail.com>
2023-04-12 10:30:10 +0500

    3. Отсортировать департаменты [#34352 #408517]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -3,13 +3,10 @@ package ru.job4j.collection;
 import java.util.Comparator;
 
 public class DepDescComp implements Comparator<String> {
-public int compare(String left, String right) {
-        String[] leftM  = left.split("/");
+    public int compare(String left, String right) {
+        String[] leftM = left.split("/");
         String[] rightM = right.split("/");
         int rslt = rightM[0].compareTo(leftM[0]);
-        if (rslt == 0) {
-            return left.compareTo(right);
-        }
-        return rslt;
-        }
+        return rslt == 0 ? left.compareTo(right) : rslt;
+    }
 }
