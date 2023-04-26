c2c02a9047008b40367b6547682da456ceb7e746
semyon <semyon.mihalich@gmail.com>
2023-04-26 10:15:30 +0500

    6. Список адресов [#110062 #412120]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -7,7 +7,7 @@ public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
        return profiles.stream()
-                .map(profile -> profile.getAddress())
+                .map(Profile::getAddress)
                 .collect(Collectors.toList());
     }
 }
