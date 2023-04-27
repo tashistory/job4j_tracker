1138dc658af5d10ce85f9cc96a56547551d9a3b1
semyon <semyon.mihalich@gmail.com>
2023-04-27 10:32:54 +0500

    17. Тестовое задание из модуля коллекции Lite переделать на Stream API. [#24260]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -7,11 +7,12 @@ import java.util.Map;
 
 /**
  * Модель для банковской системы.
-  * В классе реализованны следующие действия.
+ * В классе реализованны следующие действия.
  * 1. Регистрирует пользователя.
  * 2. Удаляет пользователя из системы.
  * 3. Добавляет пользователю банковский счет. У пользователя системы могут быть несколько счетов.
  * 4. Переводит деньги с одного банковского счета на другой счет.
+ *
  * @author Semyon
  * @version 1.0
  */
@@ -20,6 +21,7 @@ public class BankService {
 
     /**
      * Метод приниает на вход принимает пользовтаеля, создаёт ему пустой массив счетов и добавляет в карту.
+     *
      * @param user пользователь
      */
     public void addUser(User user) {
@@ -28,6 +30,7 @@ public class BankService {
 
     /**
      * Метод удаляет пользователя. На вход принимает номер паспорта.
+     *
      * @param passport номер паспорта
      * @return возвращает true если пользователь существует.
      */
@@ -38,8 +41,9 @@ public class BankService {
     /**
      * Метод добавляет счета пользавателю. На вход принимает номер паспорта и номер счёта.
      * В методе присутсвуют проверки существования пользователя и одинаковых номеров счетов.
+     *
      * @param passport номер паспорта
-     * @param account номер счёта
+     * @param account  номер счёта
      */
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
@@ -54,34 +58,34 @@ public class BankService {
     /**
      * метод реализует поиск пользователя по номеру паспорта. На вход принимает номер паспорта.
      * Если пользователь не найден, возвращается null.
+     *
      * @param passport номер паспорта
      * @return пользователь
      */
     public User findByPassport(String passport) {
-        for (User u : users.keySet()) {
-            if (u.getPassport().equals(passport)) {
-                return u;
-            }
-        }
-        return null;
+        return users.keySet()
+                .stream()
+                .filter(user -> user.getPassport().equals(passport))
+                .findFirst()
+                .orElse(null);
     }
 
     /**
      * Метод осуществляет поиск номер счёта у пользователя. На вход принимает номер паспорта и номер счёта.
      * В методе реализована проверка на наличие пользователя и номера счёта.
-     * @param passport номер паспорта
+     *
+     * @param passport  номер паспорта
      * @param requisite номер счёта
      * @return состояние счёта
      */
     public Account findByRequisite(String passport, String requisite) {
         User user = findByPassport(passport);
         if (user != null) {
-            List<Account> accounts = users.get(user);
-            for (Account account : accounts) {
-                if (account.getRequisite().equals(requisite)) {
-                    return account;
-                }
-            }
+            return users.get(user)
+                    .stream()
+                    .filter(r -> r.getRequisite().equals(requisite))
+                    .findFirst()
+                    .orElse(null);
         }
         return null;
     }
@@ -90,9 +94,10 @@ public class BankService {
      * Метод осуществляет баковскую транзакцию мкжду пользователями. Метод принимает на себя номера паспортов
      * и номера счетов участников транзакции. Метод реализует предварительную проверку состояния счёта инициатора
      * денежного перевода. Метод возвращает булеве значение завершения транзакции
-     * @param srcPassport номер паспорта инициатора транзакции
-     * @param srcRequisite номер счёта инициатора транзакции
-     * @param destPassport номер паспорта получателя денежного превода
+     *
+     * @param srcPassport   номер паспорта инициатора транзакции
+     * @param srcRequisite  номер счёта инициатора транзакции
+     * @param destPassport  номер паспорта получателя денежного превода
      * @param destRequisite номер счёта получателя денежного превода
      * @param amount
      * @return результат выполнения транзакции
@@ -112,6 +117,7 @@ public class BankService {
 
     /**
      * Метод реализует получение состояние счетов пользовтеля. Метод принимает пользователя.
+     *
      * @param user пользователь
      * @return массив счетов
      */
