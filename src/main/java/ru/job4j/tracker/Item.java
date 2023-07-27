21669607549087b3415d90f8c0a6cc4ccf83e1bb
semyon <semyon.mihalich@gmail.com>
2023-07-27 11:12:24 +0500

    2. Трекер SQL

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -2,6 +2,7 @@ package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 public class Item {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
@@ -61,5 +62,26 @@ public class Item {
 
     public void setCreated(LocalDateTime created) {
         this.created = created;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Item item = (Item) o;
+
+        if (id != item.id) return false;
+        if (!name.equals(item.name)) return false;
+        return Objects.equals(created, item.created);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + name.hashCode();
+        result = 31 * result + (created != null ? created.hashCode() : 0);
+        return result;
     }
 }
